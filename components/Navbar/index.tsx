6589f9b4ac6c446aa750f26faae03c74effc0904
Arun Murugan <arunmurugan200@gmail.com>
2021-08-04 18:31:47 +0530

    ui: improve UI of Navbar component

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,24 +1,25 @@
 import Link from "next/link";
 import { FC } from "react";
-import { Box } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 
 export interface NavbarProps {}
 
 export const Navbar: FC<NavbarProps> = (props) => {
   return (
-    <Box
+    <Flex
       color="#495464"
       as="nav"
       boxShadow="base"
       p="20px"
       width="100%"
       height="70px"
+      alignItems="center"
     >
       <Link href="/">
         <>
           <strong>M</strong>SLIDE
         </>
       </Link>
-    </Box>
+    </Flex>
   );
 };
