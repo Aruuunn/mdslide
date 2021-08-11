c7649b221103b50681ea1b4173592cdcae8ade14
Arun Murugan <arunmurugan200@gmail.com>
2021-08-11 21:26:21 +0530

    fix: type mismatch

--- lib/mapUnderscoreId.ts
+++ lib/mapUnderscoreId.ts
@@ -1,6 +1,6 @@
 import { ObjectId } from "mongodb";
 
-export function mapUnderscoreIdToId<T extends { id: string }>(
+export function mapUnderscoreIdToId<T>(
   doc: T & { _id: ObjectId }
 ) {
   return { ...doc, id: doc._id.toHexString(), _id: undefined };
