fbf70b9e0d496eb15618c9ce5282e33533ed0cff
Arun Murugan <arunmurugan200@gmail.com>
2021-08-11 18:20:28 +0530

    feat: list all presentations

---
+++ pages/api/p/all.ts
@@ -0,0 +1,23 @@
+import { Presentation } from './../../../model/presentation';
+import { getSession } from '@auth0/nextjs-auth0';
+import { withApiAuthRequired } from '@auth0/nextjs-auth0';
+import { getDb } from '../../../lib/db';
+
+
+export default withApiAuthRequired(async function (req, res) {
+    const { user } = getSession(req, res);
+
+    const db = await getDb();
+
+    const collection = db.getCollection(Presentation);
+
+    
+    const presentations = await collection
+                                    .find<Presentation>({ userEmail: user.email })
+                                    .project({  title: 1, "slides": { $slice: 1} })
+                                    .toArray()
+                                    
+                                    
+
+    res.json(presentations.map(p => ({...p, slides: undefined, coverSlide: p.slides[0]})));
+})
