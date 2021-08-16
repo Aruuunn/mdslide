964c4a52d123c2e5d126765cd5862bba59ae0669
Arun Murugan <arunmurugan200@gmail.com>
2021-08-16 10:18:54 +0530

    feat: tooltip for save indicator

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -3,16 +3,7 @@ import { debounce } from "debounce";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { FC, useState } from "react";
-import {
-  Input,
-  Flex,
-  Spacer,
-  Avatar,
-  Button,
-  Spinner,
-  Box,
-  Icon,
-} from "@chakra-ui/react";
+import { Input, Flex, Spacer, Spinner, Tooltip, Icon } from "@chakra-ui/react";
 import { AccountOptions } from "../AccountOptions";
 import { Logo } from "../Logo";
 import { useStore } from "../../lib/stores/EditorPage";
@@ -65,15 +56,19 @@ export const Navbar: FC<NavbarProps> = (props) => {
       />
       <Spacer />
       {isSaving ? (
-        <Spinner emptyColor="gray.200" width="25px" height="25px" />
+        <Tooltip label="saving.." aria-label="saving the changes">
+          <Spinner emptyColor="gray.200" width="25px" height="25px" />
+        </Tooltip>
       ) : (
-        <Icon viewBox="0 0 24 24" width="34px" color="gray.300" height="34px">
-          <path d="M0 0h24v24H0z" fill="none" />
-          <path
-            d="M19.35 10.04C18.67 6.59 15.64 4 12 4 9.11 4 6.6 5.64 5.35 8.04 2.34 8.36 0 10.91 0 14c0 3.31 2.69 6 6 6h13c2.76 0 5-2.24 5-5 0-2.64-2.05-4.78-4.65-4.96z"
-            fill="currentColor"
-          />
-        </Icon>
+        <Tooltip label="saved" aria-label="saved to cloud">
+          <Icon viewBox="0 0 24 24" width="38px" color="gray.300" height="38px">
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path
+              d="M19.35 10.04C18.67 6.59 15.64 4 12 4 9.11 4 6.6 5.64 5.35 8.04 2.34 8.36 0 10.91 0 14c0 3.31 2.69 6 6 6h13c2.76 0 5-2.24 5-5 0-2.64-2.05-4.78-4.65-4.96z"
+              fill="currentColor"
+            />
+          </Icon>
+        </Tooltip>
       )}
       <AccountOptions size="sm" />
     </Flex>
