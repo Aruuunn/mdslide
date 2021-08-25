c758ca93068c271f5deaafb004158e1765396949
Arun Murugan <arunmurugan200@gmail.com>
2021-08-25 11:34:48 +0530

    fix: service worker registration.

--- next.config.js
+++ next.config.js
@@ -4,13 +4,7 @@ const withOffline = require("next-offline");
 module.exports = removeImports(
   withOffline({
     workboxOpts: {
-      runtimeCaching: [
-        {
-          urlPattern:
-            /(www\.googleapis\.com\/webfonts\/v1\/webfonts|fonts\.gstatic\.com\/l\/font|fonts\.googleapis\.com\/css)/,
-          handler: "CacheFirst",
-        },
-      ],
+      swDest: "../public/service-worker.js",
     },
   })
 );
